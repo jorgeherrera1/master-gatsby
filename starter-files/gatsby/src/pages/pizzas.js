4970c4eb85560793c9b564acbb8299b88b7e9320
Jorge <jorge.herrera.cabrera@gmail.com>
2022-12-30 17:51:01 -0600

    Styled pizzas

--- starter-files/gatsby/src/pages/pizzas.js
+++ starter-files/gatsby/src/pages/pizzas.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { graphql } from 'gatsby';
+import PizzaList from '../components/PizzaList';
 
-export default function PizzasPage() {
+export default function PizzasPage({ data }) {
+  const pizzas = data.pizzas.nodes;
   return (
     <>
-      <p>Pizzas page</p>
+      <PizzaList pizzas={pizzas} />
     </>
   );
 }
+
+export const query = graphql`
+  query PizzaQuery {
+    pizzas: allSanityPizza {
+      nodes {
+        name
+        id
+        price
+        toppings {
+          id
+          name
+        }
+        slug {
+          current
+        }
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
