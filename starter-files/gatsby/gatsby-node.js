bc9980391945dab02ad5f3ad83a2890e2aa8870d
Jorge <jorge.herrera.cabrera@gmail.com>
2023-01-26 20:12:30 -0600

    Created individual pages for slicemasters

--- starter-files/gatsby/gatsby-node.js
+++ starter-files/gatsby/gatsby-node.js
@@ -101,6 +101,17 @@ async function turnSlicemastersIntoPages({graphql, actions}) {
     }
   `);
   // 2. turn each slicemaster into their own page
+  data.slicemasters.nodes.forEach(slicemaster => {
+    actions.createPage({
+      component: path.resolve('./src/templates/Slicemaster.js'),
+      path: `/slicemaster/${slicemaster.slug.current}`,
+      context: {
+        name: slicemaster.person,
+        slug: slicemaster.slug.current,
+      }
+    });
+  });
+
   // 3. figure out how many pages there are based on # slicemasters and how many per page
   const pageSize = parseInt(process.env.GATSBY_PAGE_SIZE);
   const pageCount = Math.ceil(data.slicemasters.totalCount / pageSize);
